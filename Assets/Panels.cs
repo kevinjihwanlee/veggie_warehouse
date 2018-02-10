aa0d2c01ae0755e32f4e72dc5bbae7f421f7e7f8
danielrthomas <danielrthomas1@gmail.com>
2018-02-10 17:43:08 -0600

    Auto formatted the panels and put them on the main scene. Also adjusted the camera

---
+++ Assets/Panels.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Panels : MonoBehaviour {
+
+	// Use this for initialization
+    void Start () {
+        RectTransform[] children = GetComponentsInChildren<RectTransform>();
+        foreach (RectTransform o in children)
+        {
+            if (o.name == "Left")
+            {
+                Vector3 pos = o.localPosition;
+                pos.x = -55;
+                o.localPosition = pos;
+            }
+            if (o.name == "Top")
+            {
+                Vector3 pos = o.localPosition;
+                pos.y = 35;
+                o.localPosition = pos;
+            }
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
