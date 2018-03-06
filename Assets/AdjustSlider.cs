d8c63da25352c29f558e90305d1429a4b353ce9e
ethlee4 <ethanlee2020@u.northwestern.edu>
2018-03-06 15:57:46 -0600

    changing the ui

---
+++ Assets/AdjustSlider.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AdjustSlider : MonoBehaviour
+{
+
+	private int _inventory;
+	private int _maxStorage;
+	
+	// Use this for initialization
+	void Start ()
+	{
+		_maxStorage = FindObjectOfType<Supply>().MaxStorage;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
