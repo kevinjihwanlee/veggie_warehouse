13641624c2a6244b6dc8c2bab37d467152587afb
Kevin J Lee <kevin.jihwan.lee@gmail.com>
2018-03-12 15:31:10 -0500

    added a basic start screen

---
+++ Assets/StartScreen.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class StartScreen : MonoBehaviour
+{
+
+	private Button _startGame;
+	
+	// Use this for initialization
+	void Start ()
+	{
+		_startGame = GameObject.Find("StartGame").GetComponent<Button>();
+		_startGame.onClick.AddListener(GoToGame);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	
+	}
+
+	void GoToGame()
+	{
+		SceneManager.LoadScene("warehouse_screen");
+	}
+}
