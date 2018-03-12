ed9a77f3f294d06855c88f9ed3f52ca6f31c1c42
Kevin J Lee <kevin.jihwan.lee@gmail.com>
2018-03-12 01:54:54 -0500

    updated tutorial with new text

--- Assets/Tutorial.cs
+++ Assets/Tutorial.cs
@@ -21,19 +21,22 @@ public class Tutorial : MonoBehaviour
 	void Start ()
 	{
 		index = 0;
-		tutorialText = new string[10];
+		tutorialText = new string[13];
 		tutorialText[0] = "At the top right, you can see what day we are on, how many lives you have, and the button that allows you to go to the next day.";
-		tutorialText[1] = "To the left of that, you can see how much money you have spent and earned the previous day.";
-		tutorialText[2] = "At the top left, you can see how much of each vegetable you have.";
-		tutorialText[3] = "Notice that there is a spoilage rate and a limit - my storage racks aren't the best. You should invest in upgrading storage later down the road, when you start making money.";
-		tutorialText[4] = "To the left, there are orders that you need to fulfill. You can stage an order and prepare it for shipment by clicking on it.";
-		tutorialText[5] =
+		tutorialText[1] = "The warehouse operates on a day-by-day cycle: orders that you stage and supply that you buy all happen at the end of day. I will explain later what that means. ";
+		tutorialText[2] = "Moving left, you can see how much money you have spent and earned the previous day.";
+		tutorialText[3] = "At the very left of that, you can see how much of each vegetable you have, divided into Current and Tomorrow's Storage.";
+		tutorialText[4] = "Notice that there is a spoilage rate and a limit - my storage racks aren't the best. You should invest in upgrading storage later down the road. ";
+		tutorialText[5] = "To the left, there are orders that you need to fulfill. You can stage an order and prepare it for shipment by clicking on it. Try clicking on it now!";
+		tutorialText[6] =
 			"The order shows the buyer name, revenue made from completing it, how much you need of each vegetable, and when you need to fulfill the order by.";
-		tutorialText[6] = "This is the buy menu for replenishing your stock. Staged orders and buying for your stock both happen at end of day, when you click on the Next Day button.";
-		tutorialText[7] =
-			"You can use this computer to communicate with your suppliers and buyers. Click on it to see what actions you can do.";
-		tutorialText[8] = "I invested my life savings into this warehouse,  so I have very high expectations. Miss 3 orders, and you're fired. I'll check up on you in 15 days, so you better impress me by then.";
-		tutorialText[9] = "I will be heading out now. Press Next and then Start to get going.";
+		tutorialText[7] = "This is the buy menu for replenishing your stock. Staged orders and buying for your stock both happen at end of day, when you click on the Next Day button.";
+		tutorialText[8] =
+			"You can use this computer to communicate with your suppliers and buyers. Click on it to see what actions you can do. ";
+		tutorialText[9] =
+			"This book contains an inventory recap that shows how much of each vegetable you have gained or lost in the past day. Click on it to see.";
+		tutorialText[10] = "I invested my life savings into this warehouse, so I have very high expectations. Miss 3 orders, and you're fired. I'll check up on you in 15 days, so you better impress me by then.";
+		tutorialText[11] = "I will be heading out now. Press Next and then Start to get going.";
 		
 		
 		_yesButton = GameObject.Find("YesButton").GetComponent<Button>();
@@ -58,22 +61,27 @@ public class Tutorial : MonoBehaviour
 		switch (index)
 		{
 			case 0: // highlight top right
+			case 1:
 				gameObject.GetComponentInChildren<Text>().text = tutorialText[index];
 				GameObject.Find("Money").gameObject.transform.localScale = new Vector3(1, 1, 1);
 				GameObject.Find("Day").gameObject.transform.localScale = new Vector3(1, 1, 1);
 				GameObject.Find("Lives").gameObject.transform.localScale = new Vector3(1, 1, 1);
 				GameObject.Find("Progress Button").gameObject.transform.localScale = new Vector3(1, 1, 1);
-				GameObject.Find("Progress Button").GetComponent<Button>().interactable = false; 
+				GameObject.Find("Progress Button").GetComponent<Button>().interactable = false;
+				//GameObject.Find("DayHighlight").gameObject.transform.localScale = new Vector3(0.5f, 0.5f, 1);
 				index++;
 				break;
-			case 1: // highlight recap panel
+			case 2: // highlight recap panel
 				gameObject.GetComponentInChildren<Text>().text = tutorialText[index];
 				GameObject.Find("Receipt").gameObject.transform.localScale = new Vector3(1, 1, 1);
+				//GameObject.Find("DayHighlight").gameObject.transform.localScale = new Vector3(0, 0, 0);
+				//GameObject.Find("RecapHighlight").gameObject.transform.localScale = new Vector3(0.5f, 0.5f, 1);
 				index++;
 				break;
-			case 2: // highlight supply
+			case 3: // highlight supply
 				gameObject.GetComponentInChildren<Text>().text = tutorialText[index];
-				//GameObject.Find("Inventory").gameObject.transform.localScale = new Vector3(1, 1, 2);
+				//GameObject.Find("RecapHighlight").gameObject.transform.localScale = new Vector3(0, 0, 0);
+				//GameObject.Find("StorageHighlight").gameObject.transform.localScale = new Vector3(0.5f, 0.5f, 1);
 				foreach (Transform child in GameObject.Find("Inventory").gameObject.transform)
 				{
 					//child.localScale = new Vector3(1, 1, 1);
@@ -87,22 +95,28 @@ public class Tutorial : MonoBehaviour
                 GameObject.FindObjectOfType<Panels>().UpdateProjected();
 				index++;
 				break;
-			case 3: // highlight spoilage rate and limit
+			case 4: // highlight spoilage rate and limit
 				gameObject.GetComponentInChildren<Text>().text = tutorialText[index];
+				//GameObject.Find("StorageHighlight").gameObject.transform.localScale = new Vector3(0, 0, 0);
+				//GameObject.Find("SpoilageHighlight").gameObject.transform.localScale = new Vector3(0.5f, 0.5f, 1);
 				GameObject.Find("Upgrade Storage").gameObject.transform.localScale = new Vector3(1, 1, 1);
 				index++;
 				break;
-			case 4: // show order
+			case 5: // show order
+				//GameObject.Find("SpoilageHighlight").gameObject.transform.localScale = new Vector3(0, 0, 0);
+				//GameObject.Find("OrderHighlight").gameObject.transform.localScale = new Vector3(0.5f, 0.5f, 1);
 				gameObject.GetComponentInChildren<Text>().text = tutorialText[index];
 				index++;
                 wm.GenerateNewOrder(wm.Orders[0]);
 				GameObject.Find("IncomingOrdersTitle").gameObject.transform.localScale = new Vector3(1, 1, 1);
 				break;
-			case 5: // highlight attributes of an order
+			case 6: // highlight attributes of an order
 				gameObject.GetComponentInChildren<Text>().text = tutorialText[index];
 				index++;
 				break;
-            case 6: // highlight buy menu
+            case 7: // highlight buy menu
+	            //GameObject.Find("OrderHighlight").gameObject.transform.localScale = new Vector3(0, 0, 0);
+	            //GameObject.Find("BuyHighlight").gameObject.transform.localScale = new Vector3(0.5f, 0.5f, 1);
                 if (wm.Orders[0].Fulfilled)
                     wm.UnstageOrder(wm.Orders[0]);
                 wm.Orders[0].active = false;
@@ -111,12 +125,20 @@ public class Tutorial : MonoBehaviour
 				gameObject.GetComponentInChildren<Text>().text = tutorialText[index];
 				index++;
 				break;
-	        case 7:
-		        GameObject.Find("Laptop").gameObject.transform.localScale = new Vector3(1,1,1);
+	        case 8:
+		        //GameObject.Find("BuyHighlight").gameObject.transform.localScale = new Vector3(0, 0, 0);
+		        //GameObject.Find("ComputerHighlight").gameObject.transform.localScale = new Vector3(0.5f, 0.5f, 1);
+		        GameObject.Find("Laptop").gameObject.transform.localScale = new Vector3(1, 1, 1);
                 gameObject.GetComponentInChildren<Text>().text = tutorialText[index];
 		        index++;
 		        break;
+			case 9:
+				//GameObject.Find("InventoryBook").gameObject.transform.localScale = new Vector3(2 ,2, 2);
+				gameObject.GetComponentInChildren<Text>().text = tutorialText[index];
+				index++;
+				break;
 			case 10:
+				//GameObject.Find("ComputerHighlight").gameObject.transform.localScale = new Vector3(0, 0, 0);
 				PlayGame();
 				break;
 			default: // otherwise just show tutorial text
@@ -130,7 +152,7 @@ public class Tutorial : MonoBehaviour
 
 	void Stage1()
 	{
-		gameObject.GetComponentInChildren<Text>().text = "I own a vegetable warehouse, and you will be managing it for me. ";
+		gameObject.GetComponentInChildren<Text>().text = "I own a vegetable warehouse, and you will be managing it for me. Let me give you a quick rundown of how things work around here. You better pay attention!";
 		_yesButton.gameObject.transform.localScale = new Vector3(0, 0, 0);
 		_noButton.gameObject.transform.localScale = new Vector3(0, 0, 0);
 		_nextButton.gameObject.transform.localScale = new Vector3(1, 1, 1);
